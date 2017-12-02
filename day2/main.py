8c80622f14672be432cebbcb575e069412af8b97
tbohne <tbohne@uos.de>
2017-12-02 20:14:46 +0100

    day2 - part2

--- day2/main.py
+++ day2/main.py
@@ -9,4 +9,14 @@ if __name__ == '__main__':
         arr = list(map(int, s.strip().split()))
         diff = max(arr) - min(arr)
         sum += diff
-    print(sum)
+    print("solution part1: ", sum)
+
+    # Part2
+    sum = 0
+    for s in input:
+        arr = list(map(int, s.strip().split()))
+        for i in arr:
+            for j in arr:
+                if i != j and i % j == 0:
+                    sum += int(i / j)
+    print("solution part2: ", sum)
