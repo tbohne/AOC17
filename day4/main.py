02a9f547687c79d8b843b3b7cb78b6c582ba0764
tbohne <tbohne@uos.de>
2017-12-05 21:40:25 +0100

    day4 - quick and dirty

--- day4/main.py
+++ day4/main.py
@@ -3,18 +3,50 @@ import math
 
 if __name__ == '__main__':
 
-    # # Saves input as string
-    # input = sys.stdin.read().strip()
-    # print(type(input))
-
-    # # Iterates over chars of the string
-    # for s in input:
-    #     print(s)
-
     # Saves a list of strings containing the lines
     input2 = sys.stdin.readlines()
     # print(type(input2))
 
-    # Iterates over the lines in the list
+    num_of_valid = 0
+
+    # Part 1
     for s in input2:
-        print(s)
+        line = s.strip().split(' ')
+        valid = True
+        arr = []
+        for i in line:
+            if i not in arr:
+                arr.append(i)
+            else:
+                valid = False
+        if valid:
+            num_of_valid += 1
+
+    print(num_of_valid)
+
+    num_of_valid = 0
+
+    # Part 2
+    for s in input2:
+        line = s.strip().split(' ')
+        valid = True
+        arr = []
+        for word in line:
+            no_anagram = True
+            for used_word in arr:
+                cnt = 0
+                for c in used_word:
+                    if c in word:
+                        cnt += 1
+                if cnt == len(word) and len(word) == len(used_word):
+                    no_anagram = False
+            if no_anagram:
+                arr.append(word)
+            else:
+                valid = False
+
+        if valid:
+            num_of_valid += 1
+        arr = []
+
+    print(num_of_valid)
