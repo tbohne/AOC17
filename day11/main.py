b093276dd78c94bc9a3cbabdd554b47c64992024
tbohne <tbohne@uos.de>
2017-12-27 23:27:04 +0100

    day11 - p1

---
+++ day11/main.py
@@ -0,0 +1,34 @@
+import sys
+import math
+
+if __name__ == '__main__':
+
+    input = sys.stdin.read().split(',')
+
+    # https://www.redblobgames.com/grids/hexagons/
+
+    coords = [0, 0, 0]
+
+    for i in input:
+        dir = i.strip()
+
+        if dir == 'n':
+            coords[1] += 1
+            coords[2] -= 1
+        elif dir == 's':
+            coords[1] -= 1
+            coords[2] += 1
+        elif dir == 'ne':
+            coords[0] += 1
+            coords[2] -= 1
+        elif dir == 'nw':
+            coords[0] -= 1
+            coords[1] += 1
+        elif dir == 'se':
+            coords[0] += 1
+            coords[1] -= 1
+        elif dir == 'sw':
+            coords[0] -= 1
+            coords[2] += 1
+
+    print("part1:",max(abs(coords[0]), abs(coords[1]), abs(coords[2])))
