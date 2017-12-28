5023276250c06c9876b2a00b33c4e591ac4cb7bf
tbohne <tbohne@uos.de>
2017-12-28 17:47:08 +0100

    day12 - p2

--- day12/main.py
+++ day12/main.py
@@ -12,7 +12,6 @@ def visit(i, dict, already_visited):
 if __name__ == '__main__':
 
     input = sys.stdin.readlines()
-
     dict = dict()
 
     for i in input:
@@ -22,7 +21,16 @@ if __name__ == '__main__':
                 tmp_list.append(j.strip())
         dict[tuple[0].strip()] = tmp_list
 
+    # Part1
     already_visited = []
     visit('0', dict, already_visited)
-
     print("part1:", len(already_visited))
+
+    # Part2
+    already_visited = []
+    groups = 0
+    for i in dict.keys():
+        if i not in already_visited:
+            visit(i, dict, already_visited)
+            groups += 1
+    print("part2:", groups)
