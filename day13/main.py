0a91628ee23f65bf7898f21105caa21ebfb963ae
tbohne <tbohne@uos.de>
2018-01-03 20:21:00 +0100

    day13 - p2 - brute force

--- day13/main.py
+++ day13/main.py
@@ -23,6 +23,48 @@ def calc_severity(depth_range):
                     up[k] = True
     print("solution part1:", severity)
 
+
+def calc_fewest_number_of_picoseconds(depth_range):
+    scanner_pos = dict()
+    up = dict()
+    for k in depth_range.keys():
+        scanner_pos[k] = 0
+        up[k] = True
+
+    # Just to enter the while loop
+    severity = 1
+    delay = 0
+
+    while severity > 0:
+        delay += 1
+        picosec = 0
+        mypos = 0
+        severity = 0
+
+        while mypos < int(last):
+
+            if str(mypos) in scanner_pos.keys() and scanner_pos[str(mypos)] == 0:
+                severity += mypos * depth_range[str(mypos)]
+
+            for k in scanner_pos.keys():
+                if up[k]:
+                    scanner_pos[k] += 1
+                    if scanner_pos[k] == depth_range[k] - 1:
+                        up[k] = False
+                else:
+                    scanner_pos[k] -= 1
+                    if scanner_pos[k] == 0:
+                        up[k] = True
+
+            if picosec > delay:
+                mypos += 1
+            picosec += 1
+        if delay % 450 == 0:
+            print(delay)
+
+    print("delay:", delay + 1)
+
+
 if __name__ == '__main__':
 
     input = sys.stdin.read().strip().replace(' ', '')
@@ -34,3 +76,4 @@ if __name__ == '__main__':
         last = tmp[0]
 
     calc_severity(depth_range)
+    calc_fewest_number_of_picoseconds(depth_range)
