dbd1ecc7feac43804deac8c091cecb98cdcae0e8
tbohne <tbohne@uos.de>
2018-02-26 20:06:54 +0100

    day17 - p2

--- day17/main.py
+++ day17/main.py
@@ -1,22 +1,27 @@
-def spinlock(steps, times):
-    seq = []
-    seq.append("0")
+def spinlock_p1(steps, times):
+    seq = ["0"]
     pos = 0
     for i in range(1, times):
-        print(i)
         pos = (pos + steps) % len(seq)
         seq.insert(pos + 1, str(i))
         pos += 1
     return seq
 
+def spinlock_p2(steps, times):
+    pos = 0
+    val = 0
+    for i in range(1, times):
+        pos = (pos + steps) % i
+        pos += 1
+        if pos == 1:
+            val = i
+    return val
+
 if __name__ == '__main__':
     steps = 394
     times_p1 = 2018
-    times_p2 = int(50e6)
-    seq = spinlock(steps, times_p1)
+    times_p2 = int(50e6) + 1
+    seq = spinlock_p1(steps, times_p1)
     idx = seq.index("2017") + 1
     print("solution part1: " + seq[idx])
-
-    seq = spinlock(steps, times_p2)
-    idx = seq.index("0") + 1
-    print("solution part2: " + seq[idx])
+    print("solution part2: " + str(spinlock_p2(steps, times_p2)))
