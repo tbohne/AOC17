cdeda87a8c15a7d03e9161aeaee33e0ac764291a
tbohne <tbohne@uos.de>
2018-02-26 19:39:47 +0100

    day17 - p2 - brute force

--- day17/main.py
+++ day17/main.py
@@ -3,6 +3,7 @@ def spinlock(steps, times):
     seq.append("0")
     pos = 0
     for i in range(1, times):
+        print(i)
         pos = (pos + steps) % len(seq)
         seq.insert(pos + 1, str(i))
         pos += 1
@@ -10,7 +11,12 @@ def spinlock(steps, times):
 
 if __name__ == '__main__':
     steps = 394
-    times = 2018
-    seq = spinlock(steps, times)
+    times_p1 = 2018
+    times_p2 = int(50e6)
+    seq = spinlock(steps, times_p1)
     idx = seq.index("2017") + 1
     print("solution part1: " + seq[idx])
+
+    seq = spinlock(steps, times_p2)
+    idx = seq.index("0") + 1
+    print("solution part2: " + seq[idx])
